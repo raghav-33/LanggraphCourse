84b3aafabbd12118edcde9c04649f9d910ad727f
raghav-33 <Raghavdevgan333@gmail.com>
2026-03-14 17:44:41 +0530

    Refactor comments for clarity on session_state
    Updated comments to clarify session_state usage and conversation history management.

--- 7.chatbot/5(a)i.chatbot_frontend.py
+++ 7.chatbot/5(a)i.chatbot_frontend.py
@@ -2,15 +2,20 @@ import streamlit as st
 from chatbot_backend import chatbot
 from langchain_core.messages import HumanMessage
 
+# Like in Normally to maintain Converstaion history , we make empty list named 'history'(i.e: history=[]) then we append user message and assitant message in dict format{'role':user/ai,content:user_message) then load entire history by loop before starting new connversation
+# But Problem in this in streamlit it got refreshed/vanish on pressing enter , But streamlit provide this type of dict known as session_state
+# session_state is special type of dict in streamlit that not got refreshed / vanish on  pressing enter , only erase when manully refresh
+
 
 CONFIG = {'configurable' : {'thread_id':'thread-1'}}
 
+# Creating session_state dict in not exist already (message_history is name of dict)
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
-# Loading Conversation History
+# Loading Conversation History then start new conversation
 for message in st.session_state['message_history']:
-    with st.chat_message(message['role']):
+    with st.chat_message(message['role']):    ## This is PreBuilt Function in Streamlit help to make you Chatbot like interface 
         st.text(message['content'])
 
 user_input = st.chat_input('Type Here')
